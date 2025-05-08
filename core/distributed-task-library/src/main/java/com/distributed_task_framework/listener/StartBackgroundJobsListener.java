9109ec502527043e0e04e3d7d69fbf1e127f9d9f
Александр Дайнеко <shokred@yandex.ru>
2025-05-09 00:15:54 +0700

    Make StartBackgroundJobsListener listen to ContextRefreshedEvent instead of ApplicationStartedEvent

--- core/distributed-task-library/src/main/java/com/distributed_task_framework/listener/StartBackgroundJobsListener.java
+++ core/distributed-task-library/src/main/java/com/distributed_task_framework/listener/StartBackgroundJobsListener.java
@@ -2,15 +2,15 @@ package com.distributed_task_framework.listener;
 
 import com.distributed_task_framework.service.BackgroundJob;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.lang.NonNull;
 
 @Slf4j
-public class StartBackgroundJobsListener implements ApplicationListener<ApplicationStartedEvent> {
+public class StartBackgroundJobsListener implements ApplicationListener<ContextRefreshedEvent> {
 
     @Override
-    public void onApplicationEvent(@NonNull ApplicationStartedEvent event) {
+    public void onApplicationEvent(@NonNull ContextRefreshedEvent event) {
         var backgroundJobs = event.getApplicationContext()
             .getBeansOfType(BackgroundJob.class);
         backgroundJobs.forEach((jobName, job) -> {
